0b8cfeb0e69823674b694f15898c157f98ffdfc8
Jesper Carlsen <jc@lendme.dk>
2020-02-03 15:57:19 +0100

    inital filestructure done

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,9 +7,7 @@
 require('./bootstrap');
 
 /**
- * Next, we will create a fresh React component instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
+ * Next, we will create a our main react component
  */
 
-require('./components/Example');
+require('./Index');
