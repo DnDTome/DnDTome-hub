694f16eba30a61af13df9692adc0ac994ab71a3a
Jesper Carlsen <jc@lendme.dk>
2020-02-03 20:29:39 +0100

    init working with react component in laravel

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// TODO: Move this to api routes file when going spa
+Route::resource('tasks', 'TaskController');
