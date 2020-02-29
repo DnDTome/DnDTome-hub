d61e7a51e33b22e360e8125f9f063809d8dcbe3e
Hailot <mightylordfe@gmail.com>
2020-02-29 12:24:08 +0100

    update to auth controller responses

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Http\Controllers\Api;
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\User;
@@ -7,8 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Validator;
 
-class AuthController extends Controller
-{
+class AuthController extends Controller {
     public $successStatus = 200;
 
     public function register(Request $request) {
@@ -20,28 +21,49 @@ class AuthController extends Controller
                 'c_password' => 'required|same:password',
             ]);
         if ($validator->fails()) {
-            return response()->json(['error'=>$validator->errors()], 401);                        }
+            return response()->json(['error' => $validator->errors()]);
+        }
         $input = $request->all();
         $input['password'] = bcrypt($input['password']);
         $user = User::create($input);
-        $success['token'] =  $user->createToken('AppName')->accessToken;
-        return response()->json($success, $this->successStatus);
+        $response = [
+            'status' => 'success',
+            'data' => [
+                'user' => $user,
+                'token' => $user->createToken('DnDTomeSite')->accessToken
+            ]
+        ];
+        return response()->json($response, $this->successStatus);
     }
 
 
-    public function login(){
-        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
+    public function login(Request $request) {
+        if (Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password
+        ])) {
             $user = Auth::user();
-            $success['token'] =  $user->createToken('AppName')->accessToken;
-            return response()->json($success, $this->successStatus);
-        } else{
-            return response()->json(['error'=>'Unauthorised'], 401);
+            $response = [
+                'token' => $user->createToken('DnDTomeSite')->accessToken,
+                'user' => $user,
+                'status' => 'success'
+            ];
+            return response()->json($response, $this->successStatus);
+        }
+        else {
+            return response()->json([
+                'status' => 'error',
+                'data' => 'Unauthorized Access'
+            ]);
         }
     }
 
     public function getUser() {
         $user = Auth::user();
-        $response['user'] = $user;
+        $response = [
+            'status' => 'success',
+            'user' => $user
+        ];
         return response()->json($response, $this->successStatus);
     }
 }
